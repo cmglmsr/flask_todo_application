8dd28797de4af7641afe86956f96e6a903528fce
cmglmsr <mgulumser@my.aci.k12.tr>
2023-03-26 03:05:12 +0300

    tasks page

--- app/app.py
+++ app/app.py
@@ -1,5 +1,4 @@
-
-import re  
+from datetime import datetime
 import os
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
@@ -51,27 +50,106 @@ def register():
         account = cursor.fetchone()
         if account:
             message = 'Choose a different username!'
-  
         elif not username or not password or not email:
             message = 'Please fill out the form!'
-
         else:
             cursor.execute('INSERT INTO User (id, username, email, password) VALUES (NULL, % s, % s, % s)', (username, email, password,))
             mysql.connection.commit()
             message = 'User successfully created!'
 
     elif request.method == 'POST':
-
         message = 'Please fill all the fields!'
     return render_template('register.html', message = message)
 
-@app.route('/tasks', methods =['GET', 'POST'])
+@app.route('/tasks', methods =['GET'])
 def tasks():
-    return "Tasks page"
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM Task ORDER BY deadline ASC')
+    tasks_list = cursor.fetchall()
+    return render_template('tasks.html', tasks_list=tasks_list)
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    # add form checks
+    # change null values of tasks
+    task_title = request.form.get("name")
+    task_description = request.form.get("description")
+    task_status = "Todo"
+    task_deadline = request.form.get("deadline")
+    task_creation_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    task_done_time = None
+    task_user_id = int(session['userid'])
+    task_type = request.form.get("tasktypes")
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("INSERT INTO Task (title, description, status, deadline, creation_time, done_time, user_id, task_type) VALUES ('{}', '{}','{}', '{}', '{}', NULL, {}, '{}')".format(task_title, task_description, task_status, task_deadline, task_creation_time, task_user_id, task_type))
+    mysql.connection.commit()
+    cursor.execute('SELECT * FROM Task ORDER BY deadline ASC')
+    tasks_list = cursor.fetchall()
+    return render_template('tasks.html', tasks_list=tasks_list)
+
+@app.route('/tasks/delete', methods=['POST'])
+def del_task():
+    task_id  = request.form.get("task_id")
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("DELETE FROM Task WHERE id={}".format(task_id))
+    mysql.connection.commit()
+    cursor.execute('SELECT * FROM Task ORDER BY deadline ASC')
+    tasks_list = cursor.fetchall()
+    return redirect(url_for('tasks'))
+
+@app.route('/tasks/edit', methods=['POST'])
+def edit_task():
+    task_id  = request.form.get("task_id")
+    task_title = request.form.get("name")
+    task_description = request.form.get("description")
+    task_deadline = request.form.get("deadline")
+    task_type = request.form.get("tasktypes")
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if task_title:
+        cursor.execute("UPDATE Task SET title = '{}' WHERE id={}".format(task_title,task_id))
+    if task_description:
+        cursor.execute("UPDATE Task SET description = '{}' WHERE id={}".format(task_description,task_id))
+    if task_deadline:
+        cursor.execute("UPDATE Task SET deadline = '{}' WHERE id={}".format(task_deadline,task_id))
+    if task_type:
+        cursor.execute("UPDATE Task SET task_type = '{}' WHERE id={}".format(task_type,task_id))
+    mysql.connection.commit()
+    return redirect(url_for('tasks'))
+
+@app.route('/tasks/edit', methods=['GET'])
+def edit_task_get():
+    task_id  = request.args.get("task_id")
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT title FROM Task WHERE id={}".format(task_id))
+    name = cursor.fetchone()["title"]
+    return render_template('edittask.html', task_name=name, task_id=task_id)
+
+@app.route('/tasks/done', methods=['POST'])
+def done_task():
+    task_id  = request.form.get("task_id")
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT status FROM Task WHERE id={}".format(task_id))
+    is_done = cursor.fetchone()["status"]
+    if is_done == "Done":
+        cursor.execute("UPDATE Task SET status = 'Todo' WHERE id={}".format(task_id))
+        cursor.execute("UPDATE Task SET done_time = NULL WHERE id={}".format(task_id))
+    else:
+        cursor.execute("UPDATE Task SET status = 'Done' WHERE id={}".format(task_id))
+        cursor.execute("UPDATE Task SET done_time = '{}' WHERE id={}".format(datetime.now(),task_id))
+    mysql.connection.commit()
+    return redirect(url_for('tasks'))
 
 @app.route('/analysis', methods =['GET', 'POST'])
 def analysis():
     return "Analysis page"
+
+
+"""
+TODO
+1 - show the completed tasks separately in main page, sorted by their completion time
+2 - make tasks special to users
+3 - analysis page
+"""
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
---
+++ sql_insert_task.sql
@@ -0,0 +1,3 @@
+USE cs353hw4db;
+INSERT INTO Task (title, description, status, deadline, creation_time, done_time, user_id, task_type) VALUES 
+('test', 'desc', 'teststat', null, null, null, 31, 'hobby');
